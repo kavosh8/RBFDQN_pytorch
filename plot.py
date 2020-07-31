c29097ff53cc834f4784fe3f1ac26c284d9de6ab
Kavosh Asadi <k8@brown.edu>
2020-07-31 15:32:40 -0400

    .

--- plot.py
+++ plot.py
@@ -33,24 +33,23 @@ ylim_up = [-100,235,300,3000,8000,3000,9350,1000,-4]
 #setting_li=[0]
 #setting_li=[0]+list(range(900,910))
 #setting_li=[0]
-labels = ['100 updates', '200 updates']
-colors = ['blue', 'orange']
+labels = ['100 updates', '200 updates', '200 updates slow target','200 updates slowest target']
+colors = ['blue', 'orange','brown','black']
 for problem in range(8):
 	plt.subplot(3,3,problem+1)
 	print(problems_name[problem])
-	for setting in [1,2]:
+	for setting in [1,2,3,4]:
 		hyper_parameter_name=10*problem+setting
 		acceptable_len=00
 		li=[]
 		for seed_num in range(10):
 			try:
 				temp=numpy.loadtxt("rbf_results/"+str(hyper_parameter_name)+"/loss_"+str(seed_num)+".txt")
-				#temp=numpy.loadtxt("rbf_results/"+str(hyper_parameter_name)+"/"+str(seed_num)+".txt")
+				temp=numpy.loadtxt("rbf_results/"+str(hyper_parameter_name)+"/"+str(seed_num)+".txt")
 				plt.plot(smooth(temp),lw=1,color=colors[setting-1])
 				if len(temp)>acceptable_len:
 					li.append(temp)
-					#plt.plot(temp)
-					print(hyper_parameter_name,seed_num,numpy.mean(temp[-10:]),len(temp))
+					#print(hyper_parameter_name,seed_num,numpy.mean(temp[-10:]),len(temp))
 			except:
 				#print("problem")
 				pass
@@ -64,6 +63,6 @@ for problem in range(8):
 		#plt.ylim([ylim_down[problem],ylim_up[problem]])
 		#plt.yticks([ylim_down[problem],ylim_up[problem]])
 	plt.title(problems_name[problem])
-	plt.legend()
+	#plt.legend()
 plt.subplots_adjust(wspace=0.5,hspace = 1)
 plt.show()
