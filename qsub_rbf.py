d0b3a9acfcef0ef37b44668288d9a441f0c1bbdc
Kavosh Asadi <k8@brown.edu>
2020-08-13 08:43:19 -0400

    .

--- qsub_rbf.py
+++ qsub_rbf.py
@@ -15,11 +15,11 @@ if not os.path.exists('pbs_files'):
 	os.makedirs('pbs_files')
 
 min_seed=0
-max_seed=10
+max_seed=20
 
 for seed_num in range(min_seed,max_seed):
 	for domain in range(3):
-		for setting in range(3,5):
+		for setting in range(1,5):
 			hyper_parameter_name=domain*10+setting
 			outfile="pbs_files/RBFDQN{}_{}.pbs".format(str(hyper_parameter_name),
 														  str(seed_num)
