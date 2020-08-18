7be22ebc882c374968a55267e557d49a1261da39
Kavosh Asadi <k8@brown.edu>
2020-08-18 12:32:57 -0400

    .

--- qsub_rbf.py
+++ qsub_rbf.py
@@ -4,7 +4,7 @@ import os.path
 bash_script = '''#!/bin/bash
 #SBATCH -n 1
 #SBATCH --mem=4G
-#SBATCH -t 48:00:00
+#SBATCH -t 72:00:00
 source ~/anaconda3/bin/activate pyt3
 echo "prog started at: $(date)"
 cd ~/RBFDQN_pytorch
@@ -22,11 +22,11 @@ if not os.path.exists('pbs_files'):
 
 
 min_seed=0
-max_seed=5
+max_seed=10
 
 for seed_num in range(min_seed,max_seed):
-	for domain in [4]:
-		for setting in range(2,10):
+	for domain in [0,1,2]:
+		for setting in range(3):
 			hyper_parameter_name=domain*10+setting
 			outfile="pbs_files/RBFDQN{}_{}.pbs".format(str(hyper_parameter_name),
 														  str(seed_num)
