57b14e27322362810aa9169223e5fab7b5f327d2
Kavosh Asadi <k8@brown.edu>
2020-08-30 09:24:40 -0400

    2k updates

--- plot.py
+++ plot.py
@@ -65,7 +65,7 @@ for problem in range(4,6):
 	#for setting in range(9):
 	
 	
-	for setting in range(4):
+	for setting in [0,1]:
 	#for setting in [0,3,4]:
 		hyper_parameter_name = str(problem) + str(setting)
 		acceptable_len = 00
